af1e30e274a5202fb7aa8ee7300bc406232d8a8a
VadimAbyshev <boss.abyshev@gmail.com>
2023-03-09 22:10:31 +0500

    New

---
+++ index.js
@@ -0,0 +1,39 @@
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__container');
+// поля формы
+let nameInput = document.querySelector('#name');
+let jobInput = document.querySelector('#description');
+let editButton = document.querySelector('.profile__button-edit');
+let NameAuthor = document.querySelector('.profile__name');
+let NameDescription = document.querySelector('.profile__description');
+let closeButton = document.querySelector('.popup__close-button');
+let editForm = document.querySelector('.form')
+
+
+
+function openPopup()  {
+  popup.classList.add('popup__opened');
+  nameInput.value =  NameAuthor.textContent;
+  jobInput.value = NameDescription.textContent;
+
+}
+
+function closePopup() {
+  popup.classList.remove('popup__opened');
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  NameAuthor.textContent = nameInput.value;
+  NameDescription.textContent = jobInput.value;
+  closePopup();
+  
+}
+editForm.addEventListener('submit', handleFormSubmit)
+
+
+
+editButton.addEventListener('click', openPopup)
+closeButton.addEventListener('click', closePopup)
+
+
